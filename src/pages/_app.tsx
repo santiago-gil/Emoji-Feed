8a83e3d6c66e18caa4ffe65122975686131cabb2
Santiago <santigilma@gmail.com>
2023-03-25 16:30:20 -0400

    Posting loading states and toasts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,17 @@
 import { type AppType } from "next/app";
-import { ClerkProvider } from '@clerk/nextjs';
-
+import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "react-hot-toast";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <ClerkProvider {...pageProps} >
+    <ClerkProvider {...pageProps}>
+      <Toaster position="bottom-center" />
       <Component {...pageProps} />
     </ClerkProvider>
   );
-}
+};
 
 export default api.withTRPC(MyApp);
